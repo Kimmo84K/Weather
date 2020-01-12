a1bb240e73db15a889d9ebeaf5b20badbccf1502
Kimmo84K <45263897+Kimmo84K@users.noreply.github.com>
2020-01-12 13:10:13 +0200

    Add files via upload

--- kaavio.py
+++ kaavio.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import time
+from matplotlib.ticker import MaxNLocator
 
 #kaavion tulostus        
 x=[]    #aika
@@ -15,10 +16,11 @@ with open("tilastot_uusi.txt","r") as csvfile:
     x.clear()
     y.clear()
     for row in plots:
+        #print(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
     #print(row[0])
     #print(row[1])
-        x.append(int(row[0]))
-        y.append(float(row[1]))
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(float(row[6]))
         
 
 plt.plot(x,y,label="Bamberg Lämpötila,$^\circ$ C")
@@ -26,6 +28,8 @@ plt.xlabel("aika")
 plt.ylabel("lämpötila,$^\circ$ C")
 plt.title("lämpötila aika")
 plt.legend()
+plt.xticks(rotation='vertical')
+
 plt.show()
 
 
@@ -38,8 +42,8 @@ with open("tilastot_uusi.txt","r") as csvfile:
     for row in plots:
     #print(row[0])
     #print(row[1])
-        x.append(int(row[0]))
-        y.append(int(row[2]))
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(int(row[7]))
         
 
 plt.plot(x,y,label="Bamberg Kosteus, %")
@@ -47,6 +51,7 @@ plt.xlabel("aika")
 plt.ylabel("Kosteus, %")
 plt.title("kosteus, aika")
 plt.legend()
+plt.xticks(rotation='vertical')
 plt.show()
 
 with open("tilastot_uusi.txt","r") as csvfile:
@@ -57,17 +62,58 @@ with open("tilastot_uusi.txt","r") as csvfile:
     for row in plots:
     #print(row[0])
     #print(row[1])
-        x.append(int(row[0]))
-        y.append(int(row[3]))
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(int(row[8]))
         
 
 plt.plot(x,y,label="Bamberg Paine, hPa")
 plt.xlabel("aika")
 plt.ylabel("Paine, hPa")
-plt.title("Paine, aika"+str(row[0]))
+plt.title("Paine, aika")
 plt.legend()
+plt.xticks(rotation='vertical')
 plt.show()
 
+with open("tilastot_uusi.txt","r") as csvfile:
+    plots = csv.reader(csvfile, delimiter=" ")
+    x.clear()
+    y.clear()
+    #print(plots)
+    for row in plots:
+    #print(row[0])
+    #print(row[1])
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(float(row[9]))
+        
+
+plt.plot(x,y,label="Bamberg tuuli nopeus, m/s")
+plt.xlabel("aika")
+plt.ylabel("tuuli nopeus, m/s")
+plt.title("Tuuli, aika")
+plt.legend()
+plt.xticks(rotation='vertical')
+plt.show()
+
+with open("tilastot_uusi.txt","r") as csvfile:
+    plots = csv.reader(csvfile, delimiter=" ")
+    x.clear()
+    y.clear()
+    #print(plots)
+    for row in plots:
+    #print(row[0])
+    #print(row[1])
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(int(row[10]))
+        
+
+plt.plot(x,y,label="Bamberg tuuli suunta, asteet")
+plt.xlabel("aika")
+plt.ylabel("tuuli suunta, asteet")
+plt.title("Tuuli suunta, aika")
+plt.legend()
+plt.xticks(rotation='vertical')
+plt.show()
+#Iitti
 with open("tilastot_uusi.txt","r") as csvfile:
     plots = csv.reader(csvfile, delimiter=" ")
     #print(plots)
@@ -76,8 +122,8 @@ with open("tilastot_uusi.txt","r") as csvfile:
     for row in plots:
     #print(row[0])
     #print(row[1])
-        x.append(int(row[0]))
-        y.append(float(row[6]))
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(float(row[12]))
         
 
 plt.plot(x,y,label="Iitti Lämpötila,$^\circ$ C")
@@ -85,6 +131,7 @@ plt.xlabel("aika")
 plt.ylabel("lämpötila,$^\circ$ C")
 plt.title("lämpötila aika")
 plt.legend()
+plt.xticks(rotation='vertical')
 plt.show()
 
 
@@ -97,8 +144,8 @@ with open("tilastot_uusi.txt","r") as csvfile:
     for row in plots:
     #print(row[0])
     #print(row[1])
-        x.append(int(row[0]))
-        y.append(int(row[7]))
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(int(row[13]))
         
 
 plt.plot(x,y,label="Iitti Kosteus, %")
@@ -106,6 +153,7 @@ plt.xlabel("aika")
 plt.ylabel("Kosteus, %")
 plt.title("kosteus, aika")
 plt.legend()
+plt.xticks(rotation='vertical')
 plt.show()
 
 with open("tilastot_uusi.txt","r") as csvfile:
@@ -116,13 +164,158 @@ with open("tilastot_uusi.txt","r") as csvfile:
     for row in plots:
     #print(row[0])
     #print(row[1])
-        x.append(int(row[0]))
-        y.append(int(row[8]))
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(int(row[14]))
         
 
 plt.plot(x,y,label="Iitti Paine, hPa")
 plt.xlabel("aika")
-plt.ylabel("Paine, hPa")
-plt.title("Paine, aika"+str(row[0]))
+plt.ylabel("Iitti Paine, hPa")
+plt.title("Iitti Paine, aika")
 plt.legend()
+plt.xticks(rotation='vertical')
 plt.show()
+
+with open("tilastot_uusi.txt","r") as csvfile:
+    plots = csv.reader(csvfile, delimiter=" ")
+    x.clear()
+    y.clear()
+    #print(plots)
+    for row in plots:
+    #print(row[0])
+    #print(row[1])
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(float(row[15]))
+        
+
+plt.plot(x,y,label="Iitti tuuli nopeus, m/s")
+plt.xlabel("aika")
+plt.ylabel("tuuli nopeus, m/s")
+plt.title("Tuuli, aika")
+plt.legend()
+plt.xticks(rotation='vertical')
+plt.show()
+
+with open("tilastot_uusi.txt","r") as csvfile:
+    plots = csv.reader(csvfile, delimiter=" ")
+    x.clear()
+    y.clear()
+    #print(plots)
+    for row in plots:
+    #print(row[0])
+    #print(row[1])
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(int(row[16]))
+        
+
+plt.plot(x,y,label="Iitti tuuli suunta, asteet")
+plt.xlabel("aika")
+plt.ylabel("tuuli suunta, asteet")
+plt.title("Tuuli suunta, aika")
+plt.legend()
+plt.xticks(rotation='vertical')
+plt.show()
+
+#Helsinki
+with open("tilastot_uusi.txt","r") as csvfile:
+    plots = csv.reader(csvfile, delimiter=" ")
+    #print(plots)
+    x.clear()
+    y.clear()
+    for row in plots:
+    #print(row[0])
+    #print(row[1])
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(float(row[18]))
+        
+plt.plot(x,y,label="Helsinki Lämpötila,$^\circ$ C")
+plt.xlabel("aika")
+plt.ylabel("lämpötila,$^\circ$ C")
+plt.title("lämpötila aika")
+plt.legend()
+plt.xticks(rotation='vertical')
+plt.show()
+
+
+with open("tilastot_uusi.txt","r") as csvfile:
+    plots = csv.reader(csvfile, delimiter=" ")
+    x.clear()
+    y.clear()
+
+    #print(plots)
+    for row in plots:
+    #print(row[0])
+    #print(row[1])
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(float(row[19]))
+        
+
+plt.plot(x,y,label="Helsinki Kosteus, %")
+plt.xlabel("aika")
+plt.ylabel("Kosteus, %")
+plt.title("kosteus, aika")
+plt.legend()
+plt.xticks(rotation='vertical')
+plt.show()
+
+with open("tilastot_uusi.txt","r") as csvfile:
+    plots = csv.reader(csvfile, delimiter=" ")
+    x.clear()
+    y.clear()
+    #print(plots)
+    for row in plots:
+    #print(row[0])
+    #print(row[1])
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(int(row[20]))
+        
+
+plt.plot(x,y,label="Helsinki Paine, hPa")
+plt.xlabel("aika")
+plt.ylabel("Helsinki Paine, hPa")
+plt.title("Helsinki Paine, aika")
+plt.legend()
+plt.xticks(rotation='vertical')
+plt.show()
+
+with open("tilastot_uusi.txt","r") as csvfile:
+    plots = csv.reader(csvfile, delimiter=" ")
+    x.clear()
+    y.clear()
+    #print(plots)
+    for row in plots:
+    #print(row[0])
+    #print(row[1])
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(float(row[21]))
+        
+
+plt.plot(x,y,label="Helsinki tuuli nopeus, m/s")
+plt.xlabel("aika")
+plt.ylabel("tuuli nopeus, m/s")
+plt.title("Tuuli, aika")
+plt.legend()
+plt.xticks(rotation='vertical')
+plt.show()
+
+with open("tilastot_uusi.txt","r") as csvfile:
+    plots = csv.reader(csvfile, delimiter=" ")
+    x.clear()
+    y.clear()
+    #print(plots)
+    for row in plots:
+    #print(row[0])
+    #print(row[1])
+        x.append(row[0]+row[1]+row[2]+row[3]+" - "+row[4])
+        y.append(int(row[22]))
+        
+
+plt.plot(x,y,label="Helsinki tuuli suunta, asteet")
+plt.xlabel("aika")
+plt.ylabel("tuuli suunta, asteet")
+plt.title("Tuuli suunta, aika")
+plt.legend()
+plt.xticks(rotation='vertical')
+plt.show()
+
+
--- saa.py
+++ saa.py
@@ -2,6 +2,7 @@ import urllib.request
 #import matplotlib.pyplot as plt
 import csv
 import time
+import json
 
 def is_number1(s):
     try: 
@@ -11,185 +12,124 @@ def is_number1(s):
         return False                
 
 
-i=0
-
 #for i in range(210):
 while True: 
         #tiedoston haku netista ja tiedostoon kirjoitus
-    try:
-        try:
-            osoite_bamberg = "http://api.openweathermap.org/data/2.5/weather?id=2952984&appid=1788aeb8578afbe75f9947b527218d64&mode=xml"
-            sivu = urllib.request.urlopen(osoite_bamberg)
-            bamberg_apu =sivu.read()
-            teksti =bamberg_apu.decode("UTF-8")
-            tiedosto_bamberg = open("bamberg.txt","w")
-            tiedosto_bamberg.write(teksti)
-            tiedosto_bamberg.close()
 
+        urlb = "http://api.openweathermap.org/data/2.5/weather?id=2952984&appid=1788aeb8578afbe75f9947b527218d64&mode=json"
+        reqb = urllib.request.Request(urlb)
 
-            osoite_iitti = "http://api.openweathermap.org/data/2.5/weather?id=656808&appid=1788aeb8578afbe75f9947b527218d64&mode=xml"
-            sivu = urllib.request.urlopen(osoite_iitti)
-            iitti_apu =sivu.read()
-            teksti =iitti_apu.decode("UTF-8")
-            tiedosto_iitti = open("iitti.txt","w")
-            tiedosto_iitti.write(teksti)
-            tiedosto_iitti.close()
+        rb = urllib.request.urlopen(reqb).read()
+        bamberg = json.loads(rb.decode('utf-8'))
+        headers_b = urllib.request.urlopen(reqb).headers
 
-        except:
-            virheet=open("virheet.txt","a")
-            virheet.write(str(aika_apu)+",ei internetyhteytta\n")
-            virheet.close()
-                    
-        #Tiedoston luku
-        data=open("bamberg.txt","r")
-        rivit_bamberg = str(data.readlines())
-        data.close()
+        #print(headers_b['date'])
+        #print(bamberg['main']['temp'])
+        #print(bamberg['main']['pressure'])         
+        #print(bamberg['main']['humidity']) 
 
-        data=open("iitti.txt","r")
-        rivit_iitti = str(data.readlines())
-        data.close()
-
-        #tiedoston siivous
-        sana = "temperature value"
-        sana2 = "lastupdate value"
-        sana3 = "humidity value"
-        sana4 = "pressure value"
-        #sana5="weather number"
-                
-
-        for alku in range(len(rivit_bamberg)):
-            loppu=alku+len(sana)
-            if rivit_bamberg[alku:loppu] ==sana:
-                #print("loytyzi:",alku,loppu)
-                alku= (rivit_bamberg[240:245])
-
-                if is_number1(alku)==True:
-                    ltila_bamberg_apu=alku
-                else:
-                    ltila_bamberg_apu=alku[:4]
-                    
-        for alku2 in range(len(rivit_bamberg)):
-            loppu2=alku2+len(sana2)
-            if rivit_bamberg[alku2:loppu2]==sana2:
-                #print("loytyzi:",alku2,loppu2)
-                aika_bamberg=(rivit_bamberg[(loppu2+2):(loppu2+2+19)])
-                aika_bamberg_apu=(aika_bamberg[8:10]+aika_bamberg[11:13]+aika_bamberg[14:16])
-                #print(aika_bamberg_apu)
-
-        for alku3 in range(len(rivit_bamberg)):
-            loppu3=alku3+len(sana3)
-            if rivit_bamberg[alku3:loppu3]==sana3:
-                #print("loytyzi:",alku2,loppu2)
-                siirto_bamberg=(rivit_bamberg[(loppu3+2):(loppu3+2+2)])
-                if is_number1(siirto_bamberg)==True:
-                    kosteus_bamberg=siirto_bamberg
-                else:
-                    kosteus_bamberg=siirto[:1]
-                    
-                #print(apu)
-
+        urli = "http://api.openweathermap.org/data/2.5/weather?id=656808&appid=1788aeb8578afbe75f9947b527218d64&mode=json"
+        reqi = urllib.request.Request(urli)
+        ri = urllib.request.urlopen(reqi).read()
+        iitti = json.loads(ri.decode('utf-8'))
             
-        for alku4 in range(len(rivit_bamberg)):
-            loppu4=alku4+len(sana4)
-            if rivit_bamberg[alku4:loppu4]==sana4:
-                #print("loytyzi:",alku2,loppu2)
-                paine_bamberg_apu=(rivit_bamberg[(loppu4+2):(loppu4+2+4)])
-                if is_number1(paine_bamberg_apu)==True:
-                    paine_bamberg=paine_bamberg_apu
-                else:
-                    paine_bamberg=paine_bamberg_apu[:3]
-                        
-                    #print(apu)
+        
+        #print(iitti['main']['temp'])
+        #print(iitti['main']['pressure'])         
+        #print(iitti['main']['humidity']) 
                     
-                                
-                #print(is_number1(s))
-                #print(is_number2(t))
-
-        if is_number1(ltila_bamberg_apu) == True & is_number1(kosteus_bamberg)== True & is_number1(aika_bamberg_apu)==True & is_number1(paine_bamberg)==True:
-            ltila_bamberg_apu2=float(ltila_bamberg_apu)
-            ltila_bamberg=ltila_bamberg_apu2-273.15
-            ltila_bamberg=round(ltila_bamberg,2)
-                    
-        #iittin tiedoston kasittely
-        for alkuk in range(len(rivit_iitti)):
-            loppuk=alkuk+len(sana)
-            if rivit_iitti[alkuk:loppuk] ==sana:
-         #       print("ltila:",alkuk,loppuk)
-                alkuk= (rivit_iitti[237:243])
-
-                if is_number1(alkuk)==True:
-                    ltila_iitti_apu=alkuk
-                else:
-                    ltila_iitti_apu=alkuk[:4]
-                #print(ltila_iitti_apu)
-                
-        for alku2k in range(len(rivit_iitti)):
-            loppu2k=alku2k+len(sana2)
-            if rivit_iitti[alku2k:loppu2k]==sana2:
-             #       print("aika:",alku2k,loppu2k)
-                aika_iitti_apu=(rivit_iitti[(loppu2k+2):(loppu2k+2+19)])
-                aika_apu_iittik=(aika_iitti_apu[8:10]+aika_iitti_apu[11:13]+aika_iitti_apu[14:16])
-                #print(aika_apu_iittik)
-
-        for alku3k in range(len(rivit_iitti)):
-            loppu3k=alku3k+len(sana3)
-            if rivit_iitti[alku3k:loppu3k]==sana3:
-                # print("kosteus:",alku3k,loppu3k)
-                kosteus_iitti_apu=(rivit_iitti[(loppu3k+2):(loppu3k+2+2)])
-                if is_number1(kosteus_iitti_apu)==True:
-                    kosteus_iitti=kosteus_iitti_apu
-                else:
-                    kosteus_iitti=kosteus_iitti_apu[:1]
-                    
-                #print(kosteus_iitti)
-
+        urlh = "http://api.openweathermap.org/data/2.5/weather?id=658226&appid=1788aeb8578afbe75f9947b527218d64&mode=json"
+        reqh = urllib.request.Request(urlh)
+        rh = urllib.request.urlopen(reqh).read()
+        helsinki = json.loads(rh.decode('utf-8'))
             
-        for alku4k in range(len(rivit_iitti)):
-            loppu4k=alku4k+len(sana4)
-            if rivit_iitti[alku4k:loppu4k]==sana4:
-                #print("paine:",alku4k,loppu4k)
-                paine_iitti_apu=(rivit_iitti[(loppu4k+2):(loppu4k+2+4)])
-                if is_number1(paine_iitti_apu)==True:
-                    paine_iitti=paine_iitti_apu
-                else:
-                    paine_iitti=paine_iitti_apu[:3]
-                        
-                #print(paine_iitti)
+        
+        #print(iitti['main']['temp'])
+        #print(iitti['main']['pressure'])         
+        #print(iitti['main']['humidity']) 
                     
-                     
 
-
-
-            
-                #print(is_number1(s))
-                #print(is_number2(t))
-
-        if is_number1(ltila_iitti_apu) == True & is_number1(kosteus_iitti)== True & is_number1(aika_apu_iittik)==True & is_number1(paine_iitti)==True:
-            ltila_iitti_apu2=float(ltila_iitti_apu)
-            ltila_iitti_apu2=ltila_iitti_apu2-273.15
-            ltila_iitti=round(ltila_iitti_apu2,2)
-
-
-
+        
+        #bamberg   
+        ltila_bamberg=round(bamberg['main']['temp']-272.15,2)
+        #print(ltila_bamberg)
+        kosteus_bamberg=bamberg['main']['humidity']
+        tuuli_nopeus_bamberg=bamberg['wind']['speed']
+        tuuli_suunta_bamberg=bamberg['wind']['deg']
+        paine_bamberg=bamberg['main']['pressure']      
+        #iitti
+        ltila_iitti=round(iitti['main']['temp']-272.15,2)
+        kosteus_iitti=iitti['main']['humidity']
+        paine_iitti=iitti['main']['pressure']
+        tuuli_nopeus_iitti=iitti['wind']['speed']
+        tuuli_suunta_iitti=iitti['wind']['deg']
+        #helsinki
+        ltila_helsinki=round(helsinki['main']['temp']-272.15,2)
+        #print(ltila_helsinki)
+        kosteus_helsinki=helsinki['main']['humidity']
+        print(kosteus_helsinki)
+        paine_helsinki=helsinki['main']['pressure']
+        print(paine_helsinki)
+        tuuli_nopeus_helsinki=helsinki['wind']['speed']
+        print(tuuli_nopeus_helsinki)
+        tuuli_suunta_helsinki=helsinki['wind']['deg']
+        print(tuuli_suunta_helsinki)
+        aika = headers_b['date']
+        #print(aika)
         #loppu sleeppi
+        print(str(aika)+" "+str(ltila_bamberg)+" "+str(kosteus_bamberg)+" "+str(paine_bamberg)+" "+str(tuuli_nopeus_bamberg)+" "+str(tuuli_suunta_bamberg)
+              +" "+" "+str(ltila_iitti)+" "+str(kosteus_iitti)+" "+str(paine_iitti)+" "+str(tuuli_nopeus_iitti)+" "+str(tuuli_suunta_iitti)
+              +" "+" "+str(ltila_helsinki)+" "+str(kosteus_helsinki)+" "+str(paine_helsinki)+" "+str(tuuli_nopeus_helsinki)+" "+str(tuuli_suunta_helsinki)+"\n")
+            
+        try:
             tulokset=open("tilastot_uusi.txt","a") 
-                    #print(ltila_bamberg_apu)
-
-            tulokset.write(str(aika_bamberg_apu)+" "+str(ltila_bamberg)+" "+str(kosteus_bamberg)+" "+str(paine_bamberg)+" "+" "+" "+str(ltila_iitti)+" "+str(kosteus_iitti)+" "+str(paine_iitti)+ "\n")
+                
+            tulokset.write(str(aika)+" "+str(ltila_bamberg)+" "+str(kosteus_bamberg)+" "+str(paine_bamberg)+" "+str(tuuli_nopeus_bamberg)+" "+str(tuuli_suunta_bamberg)
+                           +" "+" "+str(ltila_iitti)+" "+str(kosteus_iitti)+" "+str(paine_iitti)+" "+str(tuuli_nopeus_iitti)+" "+str(tuuli_suunta_iitti)
+                           +" "+" "+str(ltila_helsinki)+" "+str(kosteus_helsinki)+" "+str(paine_helsinki)+" "+str(tuuli_nopeus_helsinki)+" "+str(tuuli_suunta_helsinki)+"\n")
                 
             tulokset.close()
-        else:
-            virheet=open("virheet.txt","a")
-            virheet.write(str(aika_bamberg_apu)+", Virheellinen arvo tiedostossa\n")
-            virheet.close()
-
 
             
-    except:
-        virheet=open("virheet.txt","a")
-        virheet.write(str(aika_apu)+",ei internetyhteytta\n")
-        virheet.close()
+        except:
+            virheet=open("virheet.txt","a")
+            virheet.write(str(aika)+",virhe arvoissa\n")
+            virheet.close()
 
-    i=i+1
-    time.sleep(1800)
+        try:
+            js_tulokset = {}
+            js_tulokset['bamberg']=[]
+            js_tulokset['bamberg'].append({
+                'ltila':ltila_bamberg,
+                'kosteus':kosteus_bamberg,
+                'paine':paine_bamberg,
+                'tuuli_nopeus':tuuli_nopeus_bamberg,
+                'tuuli_suunta':tuuli_suunta_bamberg,
+                'aika': aika
+                })
+            js_tulokset['iitti']=[]
+            js_tulokset['iitti'].append({
+                'ltila':ltila_iitti,
+                'kosteus':kosteus_iitti,
+                'paine':paine_iitti,
+                'tuuli_nopeus':tuuli_nopeus_iitti,
+                'tuuli_suunta':tuuli_suunta_iitti,
+                'aika': aika
+                })
+            js_tulokset['helsinki']=[]
+            js_tulokset['helsinki'].append({
+                'ltila':ltila_helsinki,
+                'kosteus':kosteus_helsinki,
+                'paine':paine_helsinki,
+                'tuuli_nopeus':tuuli_nopeus_helsinki,
+                'tuuli_suunta':tuuli_suunta_helsinki,
+                'aika': aika
+                })
+            with open('js_tilastot.txt','a') as outfile:
+                json.dump(js_tulokset,outfile, indent=2)
+                
+        except:
+            print("virhe")
+            
+        time.sleep(1800)
     
